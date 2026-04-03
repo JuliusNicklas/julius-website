a7d9fd98e4576a150fadf30fb39a4a59d10e8317
JuliusNicklas <julius.bivren@gmail.com>
2026-04-03 22:27:11 +0200

    Added src/. Added topbar.

---
+++ src/components/tempbar.tsx
@@ -0,0 +1,13 @@
+export default function Topbar() {
+
+    return(
+        <nav className="p-2 flex justify-between bg-background border-b-1 border-foreground">
+            <div>
+                <p>Hello there</p>
+            </div>
+            <div>
+                <p>Hello there</p>
+            </div>
+        </nav>
+    );
+}
