87d5d4842d1ca89866f7c36f92f6629bf5541a9b
JuliusNicklas <julius.bivren@gmail.com>
2026-04-04 20:51:27 +0200

    Renamed Topbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Topbar from "@/components/tempbar";
+import Topbar from "@/components/Topbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
